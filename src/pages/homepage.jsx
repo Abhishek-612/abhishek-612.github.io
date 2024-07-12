6a8566456f5a0842b030368a77236d9f874029cf
Abhishek <abhirevadekar12@gmail.com>
2024-07-12 03:50:28 -0400

    minor fixes

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -93,26 +93,29 @@ const Homepage = () => {
                 <div className="subtitle homepage-subtitle">{INFO.homepage.description}</div>
               </div>
 
-              <div className="homepage-first-area-right-side">
-                <div className="homepage-image-container">
-                  <div className="homepage-image-wrapper">
-                    <img src="display-pic.png" alt="about" className="homepage-image" />
+              <div className="homepage-first-area-right-side-container">
+                <div className="homepage-first-area-right-side">
+                  <div className="homepage-image-container">
+                    <div className="homepage-image-wrapper">
+                      <img src="display-pic.png" alt="about" className="homepage-image" />
+                    </div>
                   </div>
                 </div>
+
+              <div className="homepage-socials">
+                <a href={INFO.socials.github} target="_blank" rel="noreferrer">
+                  <FontAwesomeIcon icon={faGithub} className="homepage-social-icon" />
+                </a>
+                <a href={INFO.socials.instagram} target="_blank" rel="noreferrer">
+                  <FontAwesomeIcon icon={faInstagram} className="homepage-social-icon" />
+                </a>
+                <a href={`mailto:${INFO.main.email}`} target="_blank" rel="noreferrer">
+                  <FontAwesomeIcon icon={faEnvelope} className="homepage-social-icon" />
+                </a>
+              </div>
               </div>
             </div>
 
-            <div className="homepage-socials">
-              <a href={INFO.socials.github} target="_blank" rel="noreferrer">
-                <FontAwesomeIcon icon={faGithub} className="homepage-social-icon" />
-              </a>
-              <a href={INFO.socials.instagram} target="_blank" rel="noreferrer">
-                <FontAwesomeIcon icon={faInstagram} className="homepage-social-icon" />
-              </a>
-              <a href={`mailto:${INFO.main.email}`} target="_blank" rel="noreferrer">
-                <FontAwesomeIcon icon={faEnvelope} className="homepage-social-icon" />
-              </a>
-            </div>
 
             <div className="homepage-works">
               <Works />
