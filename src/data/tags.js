368973de92dd646b3327fef1f93f746e9e652c9c
Abhishek <abhirevadekar12@gmail.com>
2024-07-16 20:32:45 -0400

    project data updated

--- src/data/tags.js
+++ src/data/tags.js
@@ -13,13 +13,24 @@ const NLP = "Natural Language Processing";
 const AUTO = "Automation";
 const IOS = "iOS";
 const C_CPP = "C/C++"
-const PY = "Python"
+const PY = "Python";
+const LINUX = "Linux";
+const WEB = "Web";
+const FLASK = "Flask";
+const DJANGO = "Django";
+const REACT = "React";
+const MONGODB = "MongoDB";
+const KAFKA = "Apache Kafka";
+const DOCKER = "Docker"
 
-export const techTags = { OP_SO, CV, NLP, AUTO, IOS, C_CPP, PY };
+export const techTags = { OP_SO, CV, NLP, AUTO, IOS, C_CPP, PY,
+    LINUX, FLASK, DJANGO, REACT, WEB, MONGODB, KAFKA, DOCKER, 
+ };
 
 export const tagColors = {
     [APP_DEV]: "#c71212", 
     [AI_ML]: "#674ea7", 
     [SYSTEMS]: "#ffa710", 
-    [MISC]: "#1b71af" 
+    [MISC]: "#1b71af",
+    [OP_SO]: "#009a0d",
   };
